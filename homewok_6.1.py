c8efa0f17f2a08d51c3a4fb400f455cfb14c1611
LesiaP92 <lesja_p92@icloud.com>
2026-05-20 18:03:27 +0300

    homework_6.1

--- homewok_6.1.py
+++ homewok_6.1.py
@@ -1,1 +1,18 @@
 import string
+
+
+user_text = input("Введіть дві літери через дифіз: ")
+start_value = user_text[0]
+end_value = user_text[2]                #1 - дефіз
+all_values = string.ascii_letters       #рядок з алфавітом
+result = ""
+start_saving = False
+for letter in all_values:
+    if letter == start_value:
+        start_saving = True
+    if start_saving == True:
+        result = result + letter
+    if letter == end_value:
+        start_saving = False
+        break
+print(result)
