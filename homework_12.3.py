1a6113fca5bc8fcbc0553fd5dd7bdea32d7c1460
LesiaP92 <lesja_p92@icloud.com>
2026-06-04 14:28:41 +0300

    homework_12.3

---
+++ homework_12.3.py
@@ -0,0 +1,11 @@
+def is_even(number):
+    if (number & 1) == 0:
+        """ умова яка порівнює число з 1 на рівні бітів &.
+        дивимось чи в кінці число парне чи ні, та перевіряємо чи дорівнює 0,якщо так то істина."""
+        return  True
+    else:
+        return False
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
